56f2e37cb09242a8ef402c9ac4c1bc027977cfde
Mykola Shumakov <mastershumakov@gmail.com>
2021-06-19 14:26:11 +0300

    First tests have been added

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -17,23 +17,43 @@ describe('Admin console page', () => {
         cy.contains('Password')
         cy.contains('Remember me')
         cy.contains('Forgot your password?')
-    })
+    });
 
     it('blocks unauthorized access', () => {
+        cy.get('#delivery_manager_email')
+            .type(email, { force: true });
+        cy.get('#delivery_manager_password')
+            .type(pass, { force: true });
         cy.get('#delivery_manager_submit_action').click();
 
         cy.get('.flashes').children()
-            .should('contain','Invalid Email or password.')
-            .and('be.visible');
-    })
-    
+            .should('contain', 'Invalid Email or password.')
+            .and("be.visible");
+    });
+
     it('allows to be logged in', () => {
+        cy.get('#delivery_manager_email')
+            .type('deliverymanager@example.com', {force: true});
+        cy.get('#delivery_manager_password')
+            .type('password', {force: true});
         cy.get('#delivery_manager_submit_action').click();
 
-        cy.get('.flashes').children()
-            .should('contain','Invalid Email or password.')
-            .and('be.visible');
-        cy.url().should();
-    })
-
+        cy.get('.flash')
+            .should('contain', 'Signed in successfully.');
+        cy.url().should('eq', 'https://intense-shore-07962.herokuapp.com/');
+        cy.get('#header').children()
+            .should('contain', 'Delivery App');
+        cy.get('#header').children()
+            .should('contain', 'Comments');
+        cy.get('#header').children()
+            .should('contain', 'Couriers');
+        cy.get('#header').children()
+            .should('contain', 'Delivery Managers');
+        cy.get('#header').children()
+            .should('contain', 'Package Assignments');
+        cy.get('#header').children()
+            .should('contain', 'Packages');
+        cy.get('#logout')
+            .should('contain', 'Logout');
+    });
 });
