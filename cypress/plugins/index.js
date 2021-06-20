807eea6d704067f6e8123a7224d68e00d62ecf28
Mykola Shumakov <mastershumakov@gmail.com>
2021-06-20 21:00:27 +0300

    Credentials have been moved into fixtures

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -20,3 +20,8 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
+
+module.exports = (on, config) => {
+    config.ignoreTestFiles = "**/examples/*.spec.js";
+    return config;
+};
